72c85ef34067bfed3d1e907ebc13f88d582d8f63
Tom Plucinski <tapluci1@gmail.com>
2021-01-09 13:16:26 -0600

    Initial commit

---
+++ src/main/java/com/commons/CarlysKitchenAPI/CarlysKitchenApiApplication.java
@@ -0,0 +1,13 @@
+package com.commons.CarlysKitchenAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarlysKitchenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarlysKitchenApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/commons/CarlysKitchenAPI/CarlysKitchenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commons.CarlysKitchenAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarlysKitchenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
